d6275f90e0cc9e99405ea0dd25eed0cd73908f7d
hrudayjangam-hub <hrudayjangam@gmail.com>
2026-06-11 14:24:35 +0530

    Add build.sh script for Render

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+echo "Installing server dependencies..."
+cd server && npm install
+echo "Installing client dependencies..."
+cd ../client && npm install
+echo "Building client..."
+npm run build
+echo "Build complete!"
