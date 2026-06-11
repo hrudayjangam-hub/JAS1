268e3d74e6529ef80d00620885fce2cc9aa436fe
hrudayjangam-hub <hrudayjangam@gmail.com>
2026-06-11 14:29:23 +0530

    Use default Node.js build with postinstall hook

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
-#!/usr/bin/env bash
-set -e
-echo "Installing server dependencies..."
-cd server && npm install
-echo "Installing client dependencies..."
-cd ../client && npm install
-echo "Building client..."
-npm run build
-echo "Build complete!"
+npm --prefix ./server install
+npm --prefix ./client install
+npm --prefix ./client run build
